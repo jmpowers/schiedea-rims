001d32546d39fe015e89546441886b54e5e4f5e0
jmpowers <icyjava@gmail.com>
2021-10-13 15:25:42 -0700

    Read in vegbiomass and survflr data

--- rims_data.R
+++ rims_data.R
@@ -1,8 +1,10 @@
 library(tidyverse)
+library(lubridate)
 library(googlesheets4)
 gsheet <- gs4_get("1pYbAnEDw2KfM34l85wlJV6pfAr1DroPj_7GjfApnCq8")
 sheet_names(gsheet)
 
+crosscol <- c("green","blue","orange","red")
 hk.pops <- set_names(c("WK","WK","WK","879WKG","892WKG","904WPG","3587WP"),
                      c("794","866","899","879","892","904","3587")) #merge Waianae Kai populations (WK)
 
@@ -18,9 +20,11 @@ add_combos <- function(x) {
          across(any_of(c("mompop", "dadpop")), recode_factor, !!!hk.pops)) 
 }
 
-# hybrids -----------------------------------------------------------------
+# crosses -----------------------------------------------------------------
 
-hybrids <- read_sheet(gsheet, "hybrids", col_types="c")
+crosses <- read_sheet(gsheet, "crosses", col_types="c") %>% 
+  mutate(species=recode(momsp, kaal="kaalae", hook="hookeri")) %>% 
+  add_combos()
 
 # seeds -------------------------------------------------------------------
 
@@ -38,5 +42,33 @@ germination <- read_sheet(gsheet, "germination", col_types="c") %>%
   summarize(planted = sum(planted), germ = sum(germ), .groups="drop") %>% # add together all the pots of one cross
   mutate(vp = germ/planted) %>% # proportion of planted seeds that germinated (viable)
   add_combos()
-  
+
+# vegbiomass --------------------------------------------------------------
+
+first_planting <- ymd("2016-03-10") #verified this is the first seed planting date for this set
+vegbiomass <- read_sheet(gsheet, "vegbiomass", col_types="c") %>% 
+  drop_na(crossid) %>% 
+  mutate(across(contains("date"), ymd),
+         #TODO from hybridbiomass.Rmd - is this the median planting date?
+         collect = collect.date - first_planting, 
+         mass=as.numeric(mass)) %>%
+  left_join(crosses) %>% 
+  add_combos()
+
+# survflr -------------------------------------------------------------
+
+survflr <- read_sheet(gsheet, "survflr", col_types="c") %>% 
+  filter(crossid != "107", plantid != "0") %>% #TODO crossid 107 is not listed in crosses 
+  #plantid=0 means seeds did not germinate
+  mutate(across(matches("date"), ymd),
+         across(all_of(c("delay","firstinflo.biomass.mg")), as.numeric),
+         alive =    ifelse(use.alive.flowered == "yes", alive == "yes", NA), #change usable statuses to boolean
+         flowered = ifelse(use.alive.flowered == "yes", na_if(flowered, "?") == "yes", NA), 
+         firstflower.date = if_else(use.firstflower == "yes", firstflower.date, as.Date(NA)),
+         firstflower = firstflower.date - first_planting) %>% 
+  left_join(crosses) %>% 
+  add_combos()
+#for flowering analysis, add filter(alive) %>% drop_na(flowered)
+
+
 
